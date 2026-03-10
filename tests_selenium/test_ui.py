bcb5c6c90dde3cd97f657a8a5eb3ea1a522ca818
jacockos <kubavlak@gmail.com>
2026-03-10 17:19:45 +0100

    oprava

--- tests_selenium/test_ui.py
+++ tests_selenium/test_ui.py
@@ -3,8 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+from selenium.webdriver.chrome.options import Options
+
 def setup_driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     driver.get("http://localhost:5173")
     return driver
 
